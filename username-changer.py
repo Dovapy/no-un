e749d2183e6667a355a94093d84c889d36e5ce5b
Dova <60954187+Dovapy@users.noreply.github.com>
2020-04-12 07:46:41 +0200

    Update username-changer.py

--- username-changer.py
+++ username-changer.py
@@ -7,23 +7,25 @@ print("Welcome to my Username changer script.")
 token = input("Enter you token : ")
 pwd = input("Enter you password : ")
 pseudo = input("Enter the pseudonym you want to put : ")
-nombrei = 1
-
-header = {"authorization": f'{token}', "Content-Type": 'application/json' }
-url = 'https://discordapp.com/api/v6/users/@me'
-payload = {"username": f"{pseudo} [{nombrei}]", "password": f'{pwd}' }
 
 
 
 
+
+
+j = 0
 i = 0
 while i < 200000:
+    header = {"authorization": f'{token}', "Content-Type": 'application/json' }
+    url = 'https://discordapp.com/api/v6/users/@me'
+    payload = {"username": f"{pseudo} [{j}]", "password": f'{pwd}' }
     r = requests.patch(url, headers=header, data=json.dumps(payload))
     r.json()
     print("\n \nRequest response : \n")
     print(r.json())
     print("\n \n")
-    print(f"Username changed to \"{pseudo} [{nombrei}]\" \n \n")
-    time.sleep(900)
+    print(f"Username changed to \"{pseudo} [{j}]\" \n \n")
     i = i + 1
-    nombrei = nombrei + 1
+    j = j + 1
+    time.sleep(900)
+    
